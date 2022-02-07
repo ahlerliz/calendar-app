49f1c290703f49d790e37531ad51fb684e657b2e
lizzy <ahlerliz@gmail.com>
2022-02-07 10:43:56 -0700

    Setting up React frontend & component design

---
+++ calendar-frontend/calendar/src/Calendar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import DayCell from "./DayCell";
+import NewEventForm from "./NewEventForm";
+
+function Calendar(){
+
+}
+
+
+export default Calendar;
---
+++ calendar-frontend/calendar/src/DayCell.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Event from "./Event";
+
+function DayCell(){
+
+}
+
+
+export default DayCell;
